5ca9761402e70e59438c2085b462b5503818db72
Bhupesh Sahu <bhupeshsahu9617@gmail.com>
2024-04-05 13:34:37 +0530

    commmit

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,14 +1,18 @@
 import React, { useState } from "react";
 
 export default function TextForm(props) {
-  const [text, setText] = useState("Enter your text");
+  const [text, setText] = useState("Enter your text here");
   const handleUpClick = () => {
     let newText = text.toUpperCase();
     setText(newText);
-  }
+  };
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
   const handleOnChange = (event) => {
     setText(event.target.value);
-  }
+  };
   return (
     <>
       <div className="container">
@@ -22,7 +26,20 @@ export default function TextForm(props) {
             onChange={handleOnChange}
           ></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Covert to upper case</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Covert to upper case
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Covert to lower case
+        </button>
+      </div>
+      <div className="container my-3">
+        <h2>Summary</h2>
+        <p>
+          {text.split(" ").length} words {text.length} characters.
+        </p>
+        <h3>Preview</h3>
+        <p>{text}</p>
       </div>
     </>
   );
